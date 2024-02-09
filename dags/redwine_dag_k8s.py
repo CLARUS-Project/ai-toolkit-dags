f9374437c4196b24258e01a89d71980feac761aa
Aitor Agirre <aagirre@ikerlan.es>
2024-02-09 09:18:17 +0000

    add credential variables (to be tested)

--- dags/redwine_dag_k8s.py
+++ dags/redwine_dag_k8s.py
@@ -40,7 +40,9 @@ def redwine_dag_over_k8s():
         "TRUE_CONNECTOR_EDGE_PORT": Variable.get("IDS_EXTERNAL_ECC_IDS_PORT"),
         "TRUE_CONNECTOR_CLOUD_IP": Variable.get("CONNECTOR_CLOUD_IP"),
         "TRUE_CONNECTOR_CLOUD_PORT": Variable.get("IDS_PROXY_PORT"),
-        "MLFLOW_ENDPOINT": Variable.get("MLFLOW_ENDPOINT")
+        "MLFLOW_ENDPOINT": Variable.get("MLFLOW_ENDPOINT"),
+        "MLFLOW_TRACKING_USERNAME": Variable.get("MLFLOW_TRACKING_USERNAME"),
+        "MLFLOW_TRACKING_PASSWORD": Variable.get("MLFLOW_TRACKING_PASSWORD")
     }
 
     volume_mount = k8s.V1VolumeMount(
