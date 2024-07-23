129e3007a50a663874edf2aabcf033b2f1f63af2
anitadell5 <aadell@ikerlan.es>
2024-07-23 09:54:56 +0200

    Removed import minio

--- src/redwine/Data/read_data.py
+++ src/redwine/Data/read_data.py
@@ -1,42 +1,36 @@
-"""
-This module provides the read_data function, which is utilized by the pipeline orchestrator (Airflow) for data ingestion. 
-The function implements the logic to ingest the data and transform it into a pandas format. If any additional auxiliary 
-functions are required to accomplish this step, they can be defined within the same script or separated into different 
-scripts and included in the Data directory.
-"""
-
 import pandas as pd
-from Data.ids_agent_client import IDSAgentClient
+from io import StringIO
+from ids_read.ids_agent_client  import IDSAgentClient
 import config
 
 def read_data() -> pd.DataFrame:
+
     """
     The function implements the logic to ingest the data and transform it into a pandas format.
 
-    In this code example, a csv file is retrieved from a url.
+    In this code example, a csv file is retrieved from a datasource.
+    If not using IDS add your own code to read the datasource
+    If using IDS uncomment the example code and replace <<Dataset Provider IP>> with the IP of the dataset provider partipant
 
     Return:
         A Pandas DataFrame representing the content of the specified file.
     """
-
     try:
-        print('Starting to read data')
-        #if not using IDS, your own code
-        #df = pd.read_csv("", delimiter=';', quotechar='"')
-
-        #if using IDS
         ids_agent_client = IDSAgentClient()
-        #Get minio enviromental variables
-        
-        ids_agent_client.read_dataset_from_ids(config.MLFLOW_EXPERIMENT, "34.251.246.165","34.250.205.215:30010","minio","minio123")
-        if ids_agent_client == False:
+        #Start transfer dataset
+        resp= ids_agent_client.get_asset_from_ids(config.MLFLOW_EXPERIMENT,"34.250.205.215:30010")
+        if resp == False:
             return None
         else:    
-            
-            df = pd.read_csv("dataset.csv", delimiter=';', quotechar='"')       
-            print('Dataset retrieved successfully from IDS')
+            #Get dataset from agent volume
+            response=ids_agent_client.get_dataset(config.MLFLOW_EXPERIMENT)
+            data = StringIO(response)
+            df = pd.read_csv(data, delimiter=';', quotechar='"')       
             return df
-        
+    
+    
+    
     except Exception as exc:
         print(f'error:  { str(exc)}') 
         return None
+       
